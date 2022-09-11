6fa1a21a141649c1ccfd0f9631bcf6557b427513
NikhilAtyali <nikhilatyali90@gmail.com>
2022-09-11 18:13:29 +0530

    Added functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,85 @@
 import './App.css';
+import React, { useState } from "react";
 
-function App() {
+
+const emojiDictionary = {
+  "😊": "Smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑": "annoyance",
+  "👨‍💻": "A boy coding",
+  "⚽": "Football",
+  "🚧": "Construction barrier",
+  "🍓": "Strawberry",
+  "🛸": "UFO",
+  "🏓": "Table tennis",
+  "🌍": "Earth",
+  "🤳": "Taking selfie",
+  "💋": "Kiss mark",
+  "🌭": "Hotdog",
+  "🛹": "Skateboard",
+  "🚂": "Locomotive",
+  "😎": "Face with sunglasses",
+  "🎁": "Wrapped gift",
+  "🎢": "Roller Coaster",
+  "🦘": "Kangaroo",
+  "🧠": "Brain",
+  "🏇": "Horse racing"
+};
+
+/*
+ * converting an object to array of keys
+ */
+const emojis = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [emoji, setEmoji] = useState("");
+  const [meaning, setmeaning] = useState("Translation will appear here");
+
+  function changeHandler(event) {
+    const inputEmoji = event.target.value;
+    setEmoji(inputEmoji);
+    if (inputEmoji in emojiDictionary) {
+      setmeaning(emojiDictionary[inputEmoji]);
+    } else {
+      setmeaning(
+        "failed to recognise this emoji, please try from the list given below"
+      );
+    }
+  }
+
+  function emojiClickHandler(inputEmoji) {
+    setmeaning(emojiDictionary[inputEmoji]);
+  }
+
   return (
     <div className="App">
-      
+      <h1 class="heading">Emoji Dictionary</h1>
+      <input
+        onChange={changeHandler}
+        value={emoji}
+        placeholder={"Enter your emoji"}
+        // style={{
+        //   padding: "1rem",
+        //   minWidth: "80%"
+        // }}
+      />
+      <h2> {emoji}</h2>
+      <h3> {meaning}</h3>
+      {emojis.map((emoji) => (
+        <span
+          onClick={() => emojiClickHandler(emoji)}
+          style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+        >
+          {" "}
+          {emoji}{" "}
+        </span>
+      ))}
     </div>
   );
 }
 
-export default App;
+
+
